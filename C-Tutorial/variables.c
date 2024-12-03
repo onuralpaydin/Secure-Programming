e2437a870c44cf742541a5cc292293d1603ba41a
Onur Alp Aydın <o.a.aydin17@gmail.com>
2024-12-03 17:09:54 +0300

    format spec. fixed

--- C-Tutorial/variables.c
+++ C-Tutorial/variables.c
@@ -8,24 +8,33 @@ int main()
 
     int intVariable = 19;
     int intVariable2 = 10;
-    float floatVariable = 19.19;
+    float floatVariable = 19.19191919191;
     char charVariable = 'a';
+    // strings are technically objects. C is not object oriented language. We will use char arrays for strings
+    char name[]="Onur Alp Aydın"; //array of characters
     // printf(intVariable); invalid conversion from 'int' to 'const char*' [-fpermissive]gcc
     /* printf(floatVariable);cannot convert 'float' to 'const char*'gcc
 argument of type "float" is incompatible with parameter of type "const char *"C/C++(167)*/
     // printf(charVariable);invalid conversion from 'char' to 'const char*' [-fpermissive]gcc
+    double doubleVariable=3.141592653589793; //8 bytes(64 bits of precision )15-16 digits.
     /*Format Specifiers
     Format specifiers are used together with the printf() function to tell the compiler what type of data the variable is storing. It is basically a placeholder for the variable value.*/
-    printf("int value: %d \n", intVariable);
-    printf("float value :%f \n", floatVariable);
-    printf("char value: %c \n", charVariable);
-    printf("int sum: %d \n", intVariable + intVariable2);
-    printf("My favorite number is: %d", 15);
     // %d or %i	int
     // %f or %F	float
     // %lf	double
     // %c	char
     // %s	Used for strings (text)
+    printf("int value: %d \n", intVariable);
+    printf("float value :%f \n", floatVariable);
+    printf("char value: %c \n", charVariable);
+    printf("int sum: %d \n", intVariable + intVariable2);
+    printf("My favorite number is: %d \n", 15);
+    printf("display string %s \n",name);
+    printf("display double variable %lf \n",doubleVariable);
+    printf("display more digits double  %0.15lf \n",doubleVariable);//more precise
+    printf("display more digits float  %0.15f \n",floatVariable);
+
+
 
     // Assign-reassign variables
     int myNum;
